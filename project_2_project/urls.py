8130086d4d2d5d97d92dc643f993b4a443e76d3f
ltran <tranln025@gmail.com>
2019-11-06 14:49:33 -0800

    Created models

--- project_2_project/urls.py
+++ project_2_project/urls.py
@@ -21,3 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('project_2_app.urls'))
 ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#         document_root=settings.MEDIA_ROOT)
