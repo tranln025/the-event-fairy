9729cb39fe371964282fe7e72727c2b0e5673682
ltran <tranln025@gmail.com>
2019-11-11 21:54:02 -0800

    Styles Event Form

--- project_2_app/forms.py
+++ project_2_app/forms.py
@@ -6,10 +6,24 @@ from . models import Event, Profile
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ('title', 'date_and_time', 'location', 'address', 'city', 'state', 'image_link', 'type', 'category')
-        widgets = {'date_and_time': DateTimeInput(attrs={'type': 'datetime-local'}),}
+        fields = ('title', 'description', 'date_and_time', 'location', 'address', 'city', 'state', 'image_link', 'type', 'category')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control col-md-6', 'style': 'margin: 0 auto'}),
+            'description': forms.TextInput(attrs={'class': 'form-control col-md-6', 'style': 'margin: 0 auto'}),
+            'date_and_time': DateTimeInput(attrs={'type': 'datetime-local', 'class': 'form-control col-md-6', 'style': 'margin: 0 auto'}),
+            'location': forms.TextInput(attrs={'class': 'form-control col-md-6', 'style': 'margin: 0 auto'}),
+            'address': forms.TextInput(attrs={'class': 'form-control col-md-6', 'style': 'margin: 0 auto'}),
+            'city': forms.TextInput(attrs={'class': 'form-control col-md-6', 'style': 'margin: 0 auto'}),
+            'state': forms.Select(attrs={'class': 'form-control col-md-6', 'style': 'margin: 0 auto'}),
+            'image_link': forms.TextInput(attrs={'class': 'form-control col-md-6', 'style': 'margin: 0 auto'}),
+            'type': forms.Select(attrs={'class': 'form-control col-md-6', 'style': 'margin: 0 auto'}),
+            'category': forms.TextInput(attrs={'class': 'form-control col-md-6', 'style': 'margin: 0 auto'}),
+        }
 
 class ProfPicForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('image_link',)
+        widgets = {
+            'image_link': forms.TextInput(attrs={'class': 'form-control col'})
+        }
