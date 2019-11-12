608901dca13b143ab7cb38726b38c9903100b8d2
DanielB <danielkbolton@gmail.com>
2019-11-11 18:36:37 -0800

    adds discription box back to event form

--- project_2_app/forms.py
+++ project_2_app/forms.py
@@ -6,7 +6,7 @@ from . models import Event, Profile
 class EventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ('title', 'date_and_time', 'location', 'address', 'city', 'state', 'image_link', 'type', 'category')
+        fields = ('title','discription', 'date_and_time', 'location', 'address', 'city', 'state', 'image_link', 'type', 'category')
         widgets = {'date_and_time': DateTimeInput(attrs={'type': 'datetime-local'}),}
 
 class ProfPicForm(forms.ModelForm):
